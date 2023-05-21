6c02c71f10f82dd4eb17e3eab6a5a7bc2aa7b2d7
iturres <emanueliturres.co@gmail.com>
2023-05-21 05:23:00 -0300

    Abstract injection of 'activitites section' markup with new -injector-function-

--- src/assets/js/markup-injectors/inject-activity-markup-template.js
+++ src/assets/js/markup-injectors/inject-activity-markup-template.js
@@ -1,8 +1,11 @@
 import activitiesdb from '../../db/activitiesdb.js';
 import createActivityMarkupTemplate from '../markup-templates/activity-markup-template.js';
+import markupInjectorWithObjectValues from './injector-function.js';
 
 const activitiesHolder = document.querySelector('[data-activities-holder]');
 
-Object.values(activitiesdb).forEach((activity) => {
-  activitiesHolder.innerHTML += createActivityMarkupTemplate(activity);
+markupInjectorWithObjectValues({
+  database: activitiesdb,
+  markupHolder: activitiesHolder,
+  markupCreator: createActivityMarkupTemplate
 });
