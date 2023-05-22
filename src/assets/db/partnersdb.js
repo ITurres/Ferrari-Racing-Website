c706f18dc825b634ff5a4e246363096950ca104d
iturres <emanueliturres.co@gmail.com>
2023-05-21 23:37:03 -0300

    Add 'partners section' -database-

---
+++ src/assets/db/partnersdb.js
@@ -0,0 +1,35 @@
+const partnersLogoSrcPath = 'src/assets/media/partner-logos/';
+const partnersdb = {
+  harman: {
+    img: {
+      src: `${partnersLogoSrcPath}harman-logo.png`,
+      alt: 'harman company logo'
+    }
+  },
+  rayban: {
+    img: {
+      src: `${partnersLogoSrcPath}Ray-Ban-Logo.png`,
+      alt: 'ray-ban brand logo'
+    }
+  },
+  shell: {
+    img: {
+      src: `${partnersLogoSrcPath}shell-logo.png`,
+      alt: 'shell corp logo'
+    }
+  },
+  vgw: {
+    img: {
+      src: `${partnersLogoSrcPath}vgw-logo.png`,
+      alt: 'vwg corp logo'
+    }
+  },
+  santander: {
+    img: {
+      src: `${partnersLogoSrcPath}santander-logo.png`,
+      alt: 'santander company logo'
+    }
+  }
+};
+
+export default partnersdb;
